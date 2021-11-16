6c692226d0c935bb567122961f10bbd9316114b7
Thijs Tijsma <thijs.tijsma@trustbit.tech>
2021-11-16 12:21:37 +0100

    Add project files.

---
+++ FSharpVersion/Library.fs
@@ -0,0 +1,23 @@
+﻿module FSharpVersion.Test
+
+open FSharp.Data
+    
+type VehiclesData = XmlProvider<Sample="data.xml">
+
+type Item =
+    { id: int
+      licensePlate: string option
+      latitude: decimal
+      longitude: decimal
+      timeStamp: string option }
+
+let data = VehiclesData.GetSample()
+
+let test =
+    data.Cars
+    |> Array.map (fun x ->
+        { id = x.TmNumber
+          licensePlate = x.NumberPlate.String
+          latitude = x.Info.Lat
+          longitude = x.Info.Lng
+          timeStamp = x.Info.Time.String })
